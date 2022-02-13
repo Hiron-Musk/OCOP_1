828220d172cf49fee2c0207213c989196c3597eb
kee <itscloudy0218@gmail.com>
2022-02-13 17:14:57 +0900

    category

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-
+# from .views import MyView
+    # , MypointView
 from . import views
 
 app_name = 'mypage'
 
 urlpatterns = [
     path('', views.mypage, name='mypage'),
+    path('mypage-chart/', views.mypage_chart, name='mypage-chart'),
+    # path('', MyView.as_view(), name='my-view'),
+    # path('', MypointView.as_view(), name='my-point-view'),
+    # path('<int:mypage_carbon_id>/', views.mypage, name='mypage'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -98,7 +98,7 @@ def question_modify(request, question_id):
             return redirect('pybo:detail', question_id=question.id)
     else:
         form = QuestionForm(instance=question)
-    context = {'form': form}
+    context = {'form': form, 'category': question.category}
     return render(request, 'pybo/question_form.html', context)
 
 @login_required(login_url='common:login')
@@ -111,7 +111,7 @@ def question_delete(request, question_id):
         messages.error(request, '삭제권한이 없습니다')
         return redirect('pybo:detail', question_id=question.id)
     question.delete()
-    return redirect('pybo:index')
+    return redirect('pybo:pybo')
 
 @login_required(login_url='common:login')
 def answer_create(request, question_id):
